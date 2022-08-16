aa80102f16c022b017dfb22a421fcf515fa3329d
Kevin Salinas <salinaskevin031@gmail.com>
2022-08-16 13:57:20 -0500

    Version 1.1.1

---
+++ Mundicasa/views.py
@@ -0,0 +1,18 @@
+from pydoc import doc
+from django.http import HttpResponse
+from django.template import Template, Context
+
+def Vista(request):
+
+    doc_externo=open("Mundicasa\Plantillas\Index.html")
+
+    plt=Template(doc_externo.read())
+
+    doc_externo.close()
+    
+    ctx=Context()
+
+    documento=plt.render(ctx)
+
+
+    return HttpResponse(documento)
